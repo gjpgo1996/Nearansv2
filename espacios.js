caf15b13f9c5a944b1425a4707a31de5cd0e3f40
Stephanie Hiyagon <mv.hiyagon@gmail.com>
2017-06-24 03:38:17 -0500

    terminado

---
+++ espacios.js
@@ -0,0 +1,34 @@
+var modal=document.getElementById('space');
+var images=document.getElementsByClassName('fotos')[0];
+var span = document.getElementById("close");
+var body=document.getElementsByTagName('body')[0];
+var btclose=document.getElementsByClassName('js-btclose')[0];
+
+//images.onclick=function captura(e){
+images.addEventListener('click',function (e){
+  e.preventDefault();
+  console.log(e.target);
+  console.log(e.target.nextElementSibling);
+  var parrafo=e.target.nextElementSibling;
+  console.log(e.target.className);
+  // colocar condicional para solo seleccionar la clase icono
+  if(e.target.className == "paisaje"){
+    // console.log(images);
+    var modalImg = document.getElementById("img01");
+    body.style.overflow="hidden";
+    modal.style.display="block";
+    modalImg.src = "img/"+e.target.alt+".jpg";
+    $('h2').text(e.target.alt);
+    $('.p1').text($(parrafo).text());
+    }
+});
+
+function cerrar (event){
+  event.preventDefault();
+  // console.log(event.target);
+    modal.style.display="none";
+    body.style.overflow="visible";
+  }
+
+span.addEventListener('click',cerrar);
+btclose.addEventListener('click',cerrar);
--- interact.js
+++ interact.js
@@ -1,7 +1,35 @@
-function myMap() {
-  var mapCanvas = document.getElementById("map");
-  var mapOptions = {
-    center: new google.maps.LatLng(51.5, -0.2), zoom: 10
-  };
-  var map = new google.maps.Map(mapCanvas, mapOptions);
+function initMap() {
+  var latitud,longitud, marcador;
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: {lat: -9.082632, lng: -84.0431127}//Coordenadas de Peru
+  });
+
+  var miUbicacion = function(posicion) {
+    latitud = posicion.coords.latitude;
+    longitud = posicion.coords.longitude;
+
+    marcador = new google.maps.Marker({
+      position: {lat:latitud, lng:longitud},
+      animation: google.maps.Animation.DROP,
+      map: map
+  });
+    map.setZoom(17);
+    map.setCenter({lat:latitud, lng:longitud});
+  }
+  var error = function (error) {
+    window.alert("Tu navegador no soporta la API de geolocalizacion");
+  }
+
+  function buscar(e) {
+    e.preventDefault();
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(miUbicacion,error);
+    }
+  }
+
+
+  document.getElementById('openMap').addEventListener("click",buscar);
+
+
 }
